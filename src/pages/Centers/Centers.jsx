5800746662bad2518c8e4d311691bab5329af62d
Cesc Greoles <antoniogreoles@Mac-Cesc-Greoles.local>
2022-12-04 11:12:03 +0100

    search-bar on centers and specialists

--- src/pages/Centers/Centers.jsx
+++ src/pages/Centers/Centers.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Centers.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { getCenters } from "../../redux/centers/centers.functions";
@@ -11,6 +11,7 @@ const Centers = () => {
     dispatch(getCenters());
   }, []);
 
+  const [query, setQuery] = useState("");
   const { user } = useSelector((state) => state.auth);
   const isAdmin = user && user.rol === "admin";
 
@@ -21,22 +22,36 @@ const Centers = () => {
           <NavLink to={"/centersCreate"}>+ </NavLink>
         </div>
       )}
+      <div className="search-centers">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="button-search-on"
+          onChange={(e) => setQuery(e.target.value)}
+        ></input>
+      </div>
       {isLoading ? (
         <img
           src="https://i.pinimg.com/originals/3d/6a/a9/3d6aa9082f3c9e285df9970dc7b762ac.gif"
           alt="loading"
         />
       ) : !error ? (
-        centers.map((center) => {
-          return (
-            <div key={center._id}>
-              <h1>{center.name}</h1>
-              <Link to={`/centers/${center._id}`} key={center._id}>
-                Ver más
-              </Link>
-            </div>
-          );
-        })
+        centers
+          .filter((center) =>
+            (center.name + center.ubication).toLowerCase().includes(query)
+          )
+          .map((center) => {
+            return (
+              <div key={center._id}>
+                <h1>{center.name}</h1>
+                <img src={center.img} alt="center-img"></img>
+                <p>{center.ubication}</p>
+                <Link to={`/centers/${center._id}`} key={center._id}>
+                  Ver más
+                </Link>
+              </div>
+            );
+          })
       ) : (
         <div>
           <h3>{error}</h3>
--- src/pages/Specialists/Specialists.jsx
+++ src/pages/Specialists/Specialists.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Specialists.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { getSpecialists } from "../../redux/specialists/specialists.functions";
@@ -18,6 +18,7 @@ const Specialists = () => {
     dispatch(getSpecialists());
   }, []);
 
+  const [query, setQuery] = useState("");
   const { user } = useSelector((state) => state.auth);
   const isAdmin = user && user.rol === "admin";
 
@@ -28,26 +29,45 @@ const Specialists = () => {
           <NavLink to={"/specialistsCreate"}>+</NavLink>
         </div>
       )}
+      <div className="search-specialist">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="button-search-on"
+          onChange={(e) => setQuery(e.target.value)}
+        ></input>
+      </div>
       {isLoading ? (
         <img
           src="https://i.pinimg.com/originals/3d/6a/a9/3d6aa9082f3c9e285df9970dc7b762ac.gif"
           alt="loading"
         />
       ) : !error ? (
-        specialists.map((specialist) => {
-          return (
-            <div key={specialist._id}>
-              <h1>{specialist.name}</h1>
-              {/* <button onClick={() => watchMore(specialist._id)}>
+        specialists
+          .filter((specialist) =>
+            (specialist.name + specialist.specialistType)
+              .toLowerCase()
+              .includes(query)
+          )
+          .map((specialist) => {
+            return (
+              <div key={specialist._id}>
+                <h1>{specialist.name}</h1>
+                <img src={specialist.img} alt="specialistImg"></img>
+                <p>{specialist.specialistType}</p>
+                {/* <button onClick={() => watchMore(specialist._id)}>
                 Detalles
               </button> */}
 
-              <Link to={`/specialists/${specialist._id}`} key={specialist._id}>
-                Ver Mas
-              </Link>
-            </div>
-          );
-        })
+                <Link
+                  to={`/specialists/${specialist._id}`}
+                  key={specialist._id}
+                >
+                  Ver Mas
+                </Link>
+              </div>
+            );
+          })
       ) : (
         <div>
           <h3>{error}</h3>
